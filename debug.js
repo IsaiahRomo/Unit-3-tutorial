59c7320f48c635fca794de61f2bdb0c51ac12690
Isaiah Romo <iromo237@west-mec.org>
2020-02-07 18:22:23 -0700

    Finished

--- debug.js
+++ debug.js
@@ -1,29 +1,30 @@
+"use srtict"
 function test(){
 
-    input = "turn on 489,959 through 759,964 turn off 820,516 through 871,914
-    var output = document.getElementById("output2");
+    var input = "turn on 489,959 through 759,964 turn off 820,516 through 871,914";
+    var output = document.getElementById("output");
 
     var list = input.split(" ");
-    let filteredList.length = [];
+    let filteredList = [];
     let instructions = [];
-    let start = []
-    let end []
+    let start = [];
+    let end = [];
     let ans = 0;
 
     var lightGrid = new Array(1000).fill(0).map(()=>new Array(1000).fill(0));
 
     //separated the list into 3 parts, an instruction, a start coord and an end coord
     for(var x in list){
-        if (list1["x"] !=== "turn" && list[x] !== through){
+        if (list[x] !== "turn" && list[x] !== "through"){
             filteredList.push(list[x]);
         }
     }
 
     //separated the filtered into its 3 important parts;
-    for(var x = 0; x < filteredList.length; x+=3){
+    for(var x = 0; x < filteredList.length; x += 3){
         instructions.push(filteredList[x]);
-        start.push(filteredList(x+1));
-        end.push(filteredlist[x+2]);
+        start.push(filteredList[x + 1]);
+        end.push(filteredList[x + 2]);
     }
     
 
@@ -39,67 +40,8 @@ function test(){
     //end is the bottom left corner in a 2d array
     //light grid is a 1000 x 1000 array of off light blubs
 
-    for(var i =0; i < instructions.length; i++){
+    for(var i = 0; i < instructions.length; i++){
         if(instructions[i] === "on"){
-            var xS = start[i][0]
-            
-
-            var xE = end[i][0]
-            var yE === end[i][1]
-
-            var tempS = 0;
-            var tempL = 0;
-
-            tempS = Math.min(xS, xE)
-            tempL = Math.MAX(xS, xE)
-
-            xS = tempS
-            xE = tempL
-
-            temps = Math.min(yS, yE)
-            tempL = Math.max(yS, yE)
-
-            yS = tempS
-            yE = TempL
-
-            for(var x = xS; x <= xE ; x++){
-                for(var y = yS; y <= yE ; y++){
-                    lightGrid[x][y] += 1;
-                }
-            }
-        }
-        else if(instructions[i] === "off"){
-            var xS = Start[i][0]
-            var yS = Start[i][1]
-
-            var xE end[i][0]
-            var yE = end[i][1]
-
-            var tempS = 0;
-            var tempL = 0;
-
-            tempS = Math.mInxS, xE);
-            tempL = Math.max(xS, xE)
-
-            xS = tempS
-            xE = tempL
-
-            temps = Math.min(yS, yE)
-            tempL = Math.max yS, yE
-
-            yS = tempS
-            yE = tempL
-
-            for(var x = xS; x <= xE ; x++){
-                for(var y = yS; y <= yE ; y++){
-                    if (lightGrid[x][y] !== 0){
-                        lightGrid[x][y] -= 1;
-                    }
-                }
-                
-            }
-        }
-        else if(instructions[i] ==== "toggle"){
             var xS = start[i][0]
             var yS = start[i][1]
 
@@ -113,10 +55,69 @@ function test(){
             tempL = Math.max(xS, xE)
 
             xS = tempS
-            xe = tempL
+            xE = tempL
 
-            tempS = Math.min(yS, yE;
-            tempL = Mathmax(yS, yE)
+            tempS = Math.min(yS, yE)
+            tempL = Math.max(yS, yE)
+
+            yS = tempS
+            yE = tempL
+
+            for(var x = xS; x <= xE ; x++){
+                for(var y = yS; y <= yE ; y++){
+                    lightGrid[x][y] += 1;
+                }
+            }
+        }
+        else if(instructions[i] === "off"){
+            var xS = start[i][0]
+            var yS = start[i][1]
+
+            var xE = end[i][0]
+            var yE = end[i][1]
+
+            var tempS = 0;
+            var tempL = 0;
+
+            tempS = Math.min(xS, xE);
+            tempL = Math.max(xS, xE)
+
+            xS = tempS
+            xE = tempL
+
+            tempS = Math.min(yS, yE);
+            tempL = Math.max(yS, yE);
+
+            yS = tempS
+            yE = tempL
+
+            for(var x = xS; x <= xE ; x++){
+                for(var y = yS; y <= yE ; y++){
+                    if (lightGrid[x][y] !== 0){
+                        lightGrid[x][y] -= 1;
+                    }
+                }
+                
+            }
+        }
+        else if(instructions[i] === "toggle"){
+            var xS = start[i][0]
+            var yS = start[i][1]
+
+            var xE = end[i][0]
+            var yE = end[i][1]
+
+            var tempS = 0;
+            var tempL = 0;
+
+            tempS = Math.min(xS, xE)
+            tempL = Math.max(xS, xE)
+
+            xS = tempS
+            xE = tempL
+
+            tempS = Math.min(yS, yE)
+            tempL = Math.max(yS, yE)
 
             yS = tempS
             yE = tempL
@@ -129,12 +130,11 @@ function test(){
         }
     }
 
-    for(var x = 0; x =< "1000" ; x++){
-        for(var y = 0; y < 1000 ; y++){
+    for(var x = 0; x <= 999 ; x++){
+        for(var y = 0; y <= 999 ; y++){
             ans += lightGrid[x][y];
         }
     }
-
 
     output.innerHTML = ans;
 
